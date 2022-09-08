1a3a2e6f97e774ec820abff030c3c39a67271d63
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-09-08 15:15:04 -0500

    event loop

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
 'use strict';
-const countriesContainer = document.querySelector('.countries');
-const renderError = function(msg){
-  countriesContainer.insertAdjacentText('beforeend', msg)
-  countriesContainer.style.opacity = 1
-}
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-      </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html)
-  countriesContainer.style.opacity = 1
-}
 
-const whereAmI = function(lat,lng){
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-  .then(res => {
-    if(!res.ok) throw new Error(`Problem with Geocoding ${res.status}`)
-    return res.json()
-  })
-  .then(data =>{
-    console.log(data)
-    console.log(`You are in ${data.city}, ${data.country}`)
-    return fetch(`https://restcountries.com/v2/name/${data.country}`)
-  })
-  .then(res=>{
-    if(!res.ok){
-      throw new Error(`Country not found ${res.status}`)
-    }
-    return res.json()
-  })
-  .then(data => renderCountry(data[0]))
-  .catch(error=>{
-    console.error(`There is an error: ${error.message}💥`)
-
-  })
-}
-
-document.querySelector('.btn-country').addEventListener('click', function(){
-  whereAmI(51.50354,-0.12768)
- 
+console.log('Test start')
+setTimeout(()=>console.log('0 sec timer', 0))
+Promise.resolve('Resolved promise 1'). then(res=>{
+  console.log(res);
 })
 
-//whereAmI(51.50354,-0.12768)
+Promise.resolve('Resolved Promised 2').then(res=>{
+  for (let i =0 ; i <10; i++){
+    console.log(res)
+  }
+})
 
+console.log(`Test end`);
+
+// code outside of any callback runs first
+// synchronous code comes first
+// Test Start
+// Test End
+//micro tasks which are callbacks 
+//related to promises has priority over the regular callback queue
+// Resolved promise 1
+// 0 sec timer // Note that you cannot rely on the 0 sec timer for precision
 
