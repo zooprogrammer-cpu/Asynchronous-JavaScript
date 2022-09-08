d5ecb88d3b13ebfaf613706bed90319a997d87ed
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-09-08 14:47:22 -0500

    Coding Challenge - Geocode

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
 'use strict';
-
-const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
-/////////////////////////////////////
 const renderError = function(msg){
-    countriesContainer.insertAdjacentText('beforeend', msg)
-    // countriesContainer.style.opacity = 1
+  countriesContainer.insertAdjacentText('beforeend', msg)
+  countriesContainer.style.opacity = 1
 }
-
 const renderCountry = function (data, className = '') {
-    const html = `
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-          </div>
-        </article>
-    `;
-    countriesContainer.insertAdjacentHTML('beforeend', html)
-    // countriesContainer.style.opacity = 1
+  const html = `
+  <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html)
+  countriesContainer.style.opacity = 1
 }
 
-//promise error handling
+const whereAmI = function(lat,lng){
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+  .then(res => {
+    if(!res.ok) throw new Error(`Problem with Geocoding ${res.status}`)
+    return res.json()
+  })
+  .then(data =>{
+    console.log(data)
+    console.log(`You are in ${data.city}, ${data.country}`)
+    return fetch(`https://restcountries.com/v2/name/${data.country}`)
+  })
+  .then(res=>{
+    if(!res.ok){
+      throw new Error(`Country not found ${res.status}`)
+    }
+    return res.json()
+  })
+  .then(data => renderCountry(data[0]))
+  .catch(error=>{
+    console.error(`There is an error: ${error.message}💥`)
 
-const getCountryData = function (country){
-    fetch(`https://restcountries.com/v2/name/${country}`)
-    .then((response)=> response.json())
-    .then((data)=>{
-        renderCountry(data[0]);
-        //using optional chaining to account for countries with no borders property
-        const neighbour = data[0].borders?.[0]
-        
-        //country 2
-        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+  })
+}
 
-    })
-    .then(response=>response.json())
-    .then(data=>renderCountry(data,'neighbour'))
-    .catch(err=>{
-       console.error(`${err}💥`);
-       renderError(`Something went wrong 💥 ${err.message}. Try again!`)
-    })
-    .finally(()=>{
-        countriesContainer.style.opacity = 1
-    })
-    
-};
-btn.addEventListener('click', function(){
-    getCountryData('germany')
+document.querySelector('.btn-country').addEventListener('click', function(){
+  whereAmI(51.50354,-0.12768)
+ 
 })
 
-
-
-
-
-
+//whereAmI(51.50354,-0.12768)
 
 
